eaf3341e724b659855d5483cc8d01ec7036d5f07
foresitegroup <mark@foresitegrp.com>
2017-10-20 13:04:18 -0500

    Latitude and longitude is automatically set if fields are left blank

--- admin/dealers-db.php
+++ admin/dealers-db.php
@@ -1,6 +1,19 @@
 <?php
 include "../inc/dbconfig.php";
 
+if ($_GET['a'] != "delete" && $_POST['latitude'] == "" || $_POST['longitude'] == "") {
+  $address = str_replace(" ", "+", $_POST['address']."+".$_POST['city']."+".$_POST['state']."+".$_POST['zip']);
+
+  $json = file_get_contents("https://maps.google.com/maps/api/geocode/json?address=$address&sensor=false$key=$GoogleAPI");
+  $json = json_decode($json);
+
+  $lat = $json->{'results'}[0]->{'geometry'}->{'location'}->{'lat'};
+  $lon = $json->{'results'}[0]->{'geometry'}->{'location'}->{'lng'};
+} else {
+  $lat = $_POST['latitude'];
+  $lon = $_POST['longitude'];
+}
+
 switch ($_GET['a']) {
   case "add":
     $mysqli->query("INSERT INTO where_to_buy (
@@ -13,26 +26,26 @@ switch ($_GET['a']) {
                 latitude,
                 longitude
                 ) VALUES (
-                '" . $_POST['customer'] . "',
-                '" . $_POST['address'] . "',
-                '" . $_POST['city'] . "',
+                '" . $mysqli->real_escape_string($_POST['customer']) . "',
+                '" . $mysqli->real_escape_string($_POST['address']) . "',
+                '" . $mysqli->real_escape_string($_POST['city']) . "',
                 '" . $_POST['state'] . "',
                 '" . $_POST['zip'] . "',
                 '" . $_POST['telephone'] . "',
-                '" . $_POST['latitude'] . "',
-                '" . $_POST['longitude'] . "'
+                '" . $lat . "',
+                '" . $lon . "'
                 )");
     break;
   case "edit":
     $mysqli->query("UPDATE where_to_buy SET
-                customer = '" . $_POST['customer'] . "',
-                address = '" . $_POST['address'] . "',
-                city = '" . $_POST['city'] . "',
+                customer = '" . $mysqli->real_escape_string($_POST['customer']) . "',
+                address = '" . $mysqli->real_escape_string($_POST['address']) . "',
+                city = '" . $mysqli->real_escape_string($_POST['city']) . "',
                 state = '" . $_POST['state'] . "',
                 zip = '" . $_POST['zip'] . "',
                 telephone = '" . $_POST['telephone'] . "',
-                latitude = '" . $_POST['latitude'] . "',
-                longitude = '" . $_POST['longitude'] . "'
+                latitude = '" . $lat . "',
+                longitude = '" . $lon . "'
                 WHERE id = '" . $_POST['id'] . "'");
     break;
   case "delete":
